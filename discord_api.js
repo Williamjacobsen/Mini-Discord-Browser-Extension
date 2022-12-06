70928b1c1483ae5111283a802a96c2bd80d19715
Williamjacobsen <71521465+Williamjacobsen@users.noreply.github.com>
2022-12-06 21:06:21 +0100

    bug fix, a waste of 8 hours do to bad discord docs
    suck a dick

--- discord_api.js
+++ discord_api.js
@@ -1,3 +1,5 @@
+// to look at => https://github.com/PasinduDushan/Discord-Gateway-Example/blob/main/index.js
+
 const Request = require("request");
 const WebSocket = require("ws");
 const fs = require("fs");
@@ -26,60 +28,57 @@ const ID = "1047213484309483582";
 */
 const AUTHORIZATION = config["user"]["Authorization"];
 
+let interval = 0;
+
 const Payload = {
   op: 2,
   d: {
     token: AUTHORIZATION,
-    intents: 513,
+    intents: 131071,
     properties: {
-      os: "linux",
-      browser: "Firefox",
-      device: "pc",
+      $os: "linux",
+      $browser: "chrome",
+      $device: "chrome",
+    },
+    presence: {
+      // omfg, the docs didn't mention this,
+      // neither did tutorials,
+      // but it fix it...
+      // after 8 fucking hours :(
+      activities: [
+        {
+          name: "Cards Against Humanity",
+          type: 0,
+        },
+      ],
+      status: "dnd",
+      since: 91879201,
+      afk: false,
     },
   },
 };
 
-function heartbeat(ms) {
+const heartbeat = (ms) => {
+  // had a bug for 8 hours, it should be a method not a function... Plz kill me...
   return setInterval(() => {
     ws.send(JSON.stringify({ op: 1, d: null }));
   }, ms);
-}
+};
 
 ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json");
+
+/*
+  Explain, future me...
+*/
 
 ws.addEventListener("open", (event) => {
   ws.send(JSON.stringify(Payload));
 });
 
-ws.addEventListener("message", (event) => {
-  if (JSON.parse(event.data)["op"] == 10) {
-    const heartbeat_interval = JSON.parse(event.data)["d"][
-      "heartbeat_interval"
-    ];
-    ws.send(JSON.stringify(Payload));
-    heartbeat(heartbeat_interval);
+ws.on("message", function incoming(event) {
+  event = JSON.parse(event);
+  console.log(event);
+  if (event["op"] == 10) {
+    interval = heartbeat(event["d"]["heartbeat_interval"]);
   }
-  console.log(JSON.parse(event.data));
-  //if (JSON.parse(event.data)["t"] == "MESSAGE_CREATED") {}
 });
-
-/*
-const Payload = {
-  content: "sus",
-};
-
-Request.post(
-  URL,
-  {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: AUTHORIZATION,
-    },
-    body: JSON.stringify(Payload),
-  },
-  function (err, res, body) {
-    console.log(err);
-    console.log(body);
-  }
-);
-*/
