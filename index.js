d2a3125e83ae08c86f7ea785053ed272094bfe2e
Williamjacobsen <71521465+Williamjacobsen@users.noreply.github.com>
2022-12-09 16:37:22 +0100

    more ui, sidebar options

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//document.getElementById("sus").innerHTML = "heawheawhe";
+document.getElementById("send_message").addEventListener("click", () => {
+  document.body.style.width = "500px";
+  const optionMenu = document.createElement("div");
+  optionMenu.className = "option_menu";
+  document.body.appendChild(optionMenu);
+});
